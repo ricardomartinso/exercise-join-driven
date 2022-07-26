656f75c9c67ae4cdc87690f273a25d64649c1175
ricardomartinso <ricardomartinso.dev@gmail.com>
2022-07-26 19:43:47 -0300

    4 exercise done

--- exercise.sql
+++ exercise.sql
@@ -6,4 +6,4 @@ SELECT testimonials.id, u1.name AS writer, u2.name AS recipient, testimonials.me
 SELECT users.id, users.name, courses.name AS course, schools.name AS school, educations."endDate" FROM educations JOIN courses ON courses.id = educations."courseId" JOIN schools ON educations."schoolId" = schools.id JOIN users ON users.id = educations."userId" WHERE status = 'finished' AND "userId" = 30;
 
 
-
+SELECT users.id, users.name, roles.name AS role, companies.name AS company, experiences."startDate" FROM experiences JOIN users ON users.id = experiences."userId" JOIN roles ON roles.id = experiences."roleId" JOIN companies ON companies.id = experiences."companyId" WHERE "userId" = 50 AND "endDate" IS NULL;
